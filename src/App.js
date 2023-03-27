39a13d625948c0bcd2f955f71bebd4833248d6aa
xojohnxo <johnmavridis1@gmail.com>
2023-03-27 22:13:14 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import Sidebar from './components/Sidebar'
+import Topbar from "./components/Topbar";
+import "./App.css";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserList from "./pages/UserList";
+import User from "./pages/User";
+import NewUser from "./pages/NewUser";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
+import NewProduct from "./pages/NewProduct";
+import Login from "./pages/Login";
+import React, { useState } from 'react';
+import { Navigate } from 'react-router-dom/dist';
+
+function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const handleLogin = (username, password) => {
+
+    setIsLoggedIn(true); }
+
+  return (
+    <Router>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element= {isLoggedIn ? <Navigate to="/home" /> : <Login onLogin={handleLogin} />} />
+          <Route path="/home" element= {isLoggedIn ? <Home /> : <Navigate to="/" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/users" element={<UserList />} />
+          <Route path="/user/:userId" element={<User />} />
+          <Route path="/newUser" element={<NewUser />} />
+          <Route path="/products" element={<ProductList />} />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/newproduct" element={<NewProduct />} />
+        </Routes>
+      </div>
+      </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,81 @@
+import '../css/page/login.css'
+import { useState, useEffect} from "react";
+import { user } from '../dummyData';
+
+
+function Login(){
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [errorMessages, setErrorMessages] = useState("");
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+
+    const errors = {
+        username: "Wrong username",
+        password: "Wrong password",
+    };
+    
+    const currentUser = user.find((user) => user.username === username);
+    
+      useEffect(() => {
+        if (!!currentUser) {
+          setErrorMessages((prevErrors) => ({
+            ...prevErrors,
+            username: errors.username,
+          }));
+        } else {
+          setErrorMessages((prevErrors) => ({
+            ...prevErrors,
+            username: '',
+          }));
+        }
+    
+        if (currentUser && currentUser.password !== password) {
+          setErrorMessages((prevErrors) => ({
+            ...prevErrors,
+            password: errors.password,
+          }));
+        } else {
+          setErrorMessages((prevErrors) => ({
+            ...prevErrors,
+            password: '',
+          }));
+        }
+      }, [currentUser, password, username]);
+    
+    const handleSubmit = (e) => {
+        e.preventDefault(); //no reloading
+        setIsLoggedIn(true);
+        localStorage.setItem('currentUser', JSON.stringify(currentUser));
+    };
+
+    const renderErrorMsg = (name) =>
+    name === errorMessages.name && (
+      <p className="error_msg">{errorMessages.message}</p>
+    );
+
+    return(
+        <form onSubmit={handleSubmit}>
+        <div className="inputs">
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => {
+                setUsername(e.target.value)}
+            }
+          />
+          {renderErrorMsg("username")}
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {renderErrorMsg("password")}
+        </div>
+        <input type="submit" value="Log In" className="login_button" />
+      </form>
+    )
+}
+export default Login;
